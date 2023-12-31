e5ae9956f81f2f851ef0d97e76ad84fb72a656fe
Hammed <hamheadonofficia@gmail.com>
2023-12-31 19:21:31 +0000

    started working on the server gateway

--- extra_functions.py
+++ extra_functions.py
@@ -1,9 +1,39 @@
 import socket
+import pyrebase
 
+def connect_to_fire():
+    firebaseConfig = {
+        "apiKey": "AIzaSyCoCxbmJVfVd3ZgD5Mlozi5jDoW4t7_j-Q",
+        "authDomain": "hnd-fire.firebaseapp.com",
+        "databaseURL": "https://hnd-fire-default-rtdb.europe-west1.firebasedatabase.app",
+        "projectId": "hnd-fire",
+        "storageBucket": "hnd-fire.appspot.com",
+        "messagingSenderId": "867594392762",
+        "appId": "1:867594392762:web:7ae45244a2f6f812741e83",
+        "measurementId": "G-2VWFXC9HP2"
+    }
+
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db = firebase.database()
+    return db
+
+def grab_server_address():
+    return connect_to_fire().child("server_addr").get().val()
 
 def connect_to_server():
     port = 8080
-    HOST = socket.gethostbyname(socket.gethostname())
+    HOST = grab_server_address()
     ADDR = (HOST, port)
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(ADDR)
+    return s
+
+def send_command(msg):
+    message = msg.encode("utf-8")
+    msg_length = len(message)
+    send_length = str(msg_length).encode("utf-8")
+    client = connect_to_server()
+    client.send(send_length)
+    client.send(message)
+
+send_command("Hello")
--- server.py
+++ server.py
@@ -1,10 +1,27 @@
 import socket
 from threading import Thread
-
+import pyrebase
 players_online = {}
 
+
+def connect_to_fire():
+    firebaseConfig = {
+        "apiKey": "AIzaSyCoCxbmJVfVd3ZgD5Mlozi5jDoW4t7_j-Q",
+        "authDomain": "hnd-fire.firebaseapp.com",
+        "databaseURL": "https://hnd-fire-default-rtdb.europe-west1.firebasedatabase.app",
+        "projectId": "hnd-fire",
+        "storageBucket": "hnd-fire.appspot.com",
+        "messagingSenderId": "867594392762",
+        "appId": "1:867594392762:web:7ae45244a2f6f812741e83",
+        "measurementId": "G-2VWFXC9HP2"
+    }
+
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db = firebase.database()
+    return db
+
 def handle_client(conn, addr, *args):
-    print()
+    print(f"Connected by: {addr}")
     while True:
         data = conn.recv(HEADER).decode(FORMAT)
         if not data:
@@ -36,11 +53,14 @@ ADDR = (HOST, PORT)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 server.listen()
-server_is_active = False
+server_is_active = True
 
+connect_to_fire().update({"server_addr": socket.gethostbyname(socket.gethostname())})
+print("Server has started")
 while server_is_active:
     conn, addr = server.accept()
     thread = Thread(target=handle_client, args=(conn, addr))
     thread.start()
+    print(f"Connected received by: {addr}")
 
 
